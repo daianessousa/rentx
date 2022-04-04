2789ddeafdf5ac30afa827fdde81b2ebe93f02e6
daianessousa <daisousa.c@gmail.com>
2022-04-04 09:24:22 -0300

    ADD authenticate

--- src/database/migrations/1649011945585-CreateUsers.ts
+++ src/database/migrations/1649011945585-CreateUsers.ts
@@ -17,11 +17,6 @@ export class CreateUsers1649011945585 implements MigrationInterface {
             type: "varchar",
           },
           {
-            name: "username",
-            type: "varchar",
-            isUnique: true,
-          },
-          {
             name: "password",
             type: "varchar",
           },
